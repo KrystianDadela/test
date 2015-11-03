78faf5dab2eb5edb025af2287f96787653cfe8ad
KrystianDadela <12154008@studentmail.ul.ie>
2015-11-03 19:29:06 +0000

    small changes

---
+++ src/main/java/Constraints.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Constraints {
+
+    public void readConstraints() throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("./src/main/java/Constraints.txt"));
+        String line;
+        int count = 0;
+        List<String> temps = new ArrayList<String>();
+        while ((line = br.readLine()) != null) {
+
+            temps.add(line);
+
+
+            count++;
+        }
+        br.close();
+
+
+        for (int index = 0; index < count; index++) {
+            System.out.println(temps.get(index));
+        }
+
+
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,9 +5,11 @@ public class main{
     public static void main (String [] args)throws IOException{
 
 
-
+        Constraints const1 = new Constraints();
         GenerateFixtures user1 = new GenerateFixtures();
         user1.GenerateTable();
+        const1.readConstraints();
+
     }
 }
 
